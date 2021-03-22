129b3801d19401305db20c8e39992a4eee28083b
isavasquez <65193842+isavasquez@users.noreply.github.com>
2021-03-22 18:41:31 -0500

    Create codigo_comentariossep.R
    codigo para separar los cementarios ya valide y son hasta 31

---
+++ Grupo Web Scraping/codigo_comentariossep.R
@@ -0,0 +1,14 @@
+
+datos <- readRDS("C:/Users/USUARIO_PC/Documents/GitHub/proyecto_FOCUS/Grupo Web Scraping/restaurant_trip_advisor.rds") 
+
+library(tidyverse)
+dat<- data.frame(id=datos$id, comentarios= datos$Comentarios)
+
+dat<-dat %>%
+  separate(comentarios,c("coment1","coment2","coment3","coment4","coment5","coment6",
+                     "coment7","coment8","coment9","coment10","coment11",
+                     "coment12","coment13","coment14","coment15","coment16",
+                     "coment17","coment18","coment19","coment20","coment21","coment22",
+                     "coment23","coment24","coment25","coment26","coment27"
+                     ,"coment29","coment30","coment31"),
+           sep= "~")
