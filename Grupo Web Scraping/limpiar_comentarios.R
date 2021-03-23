82a9f62d5ed84a4da55a4b3783a50fa8c5117cf3
Jose Navarro De la Cruz <67207312+JoseNavarroDelaCruz@users.noreply.github.com>
2021-03-22 21:45:36 -0500

    actualizacion codigo para limpiar dataset restaurantes

--- Grupo Web Scraping/limpiar_comentarios.R
+++ Grupo Web Scraping/limpiar_comentarios.R
@@ -5,11 +5,14 @@ library(tidyverse)
 library(RColorBrewer)
 library(plyr)
 library(ggplot2)
-library(sentiment)
 library(rtweet)
 library(wordcloud)
 library(stringr)
 library(tm)
+library(devtools)
+install_url("http://cran.r-project.org/src/contrib/Archive/sentiment/sentiment_0.2.tar.gz")
+require(sentiment)
+ls("package:sentiment")
 
 #### Cargamos los datos ####
 data <- readRDS("restaurant_trip_advisor.rds")
@@ -73,13 +76,30 @@ data$comentarios <- iconv(data$comentarios, 'UTF-8', 'ASCII') # Quitamos los emo
 data$comentarios <- gsub("[ \t]{2,}", "", data$comentarios) # Quitamos tabuladores
 data$comentarios <- gsub("^\\s+|\\s+$", "", data$comentarios) # Quitamos tabuladores x2
 data$comentarios <- tolower(data$comentarios) # Pasamos todo a minúsculas
-data <- data[!is.na(data$comentarios), ] # Removemos aquellas filas que tengan NAs
 head(data$comentarios, 3)
+
+#write.csv(data, "restaurant_tripAdvisor_corregido.csv")
+
 
 
 ##### Procedemos con el análisis de los textos #####
 
-corpus_restaurantes <- Corpus(VectorSource(data$comentarios))
+comentarios <- read.csv("comentarios_separados.csv") %>% select(-1)
+
+### Corregimos los comentarios
+comentarios <- gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "",comentarios) # Quitamos Retweets
+comentarios<- gsub("@\\w+", "", comentarios) # Quitamos mencion a personas
+comentarios<-  gsub("\\bhttp[a-zA-Z0-9]*\\b", "", comentarios) # Quitamos los links html
+comentarios <- gsub("[^a-zA-Z0-9 ]", "", comentarios) # Se quietan carácteres no alfanuméricos
+comentarios<- gsub("[[:punct:]]", "", comentarios) # Quitamos puntuación
+comentarios <- gsub("amp ", "", comentarios)# Quitamos la palabra amp
+comentarios <-  gsub("\\btco[a-zA-Z0-9]*\\b", "", comentarios) # Quitamos los Toc
+comentarios <- iconv(comentarios, 'UTF-8', 'ASCII') # Quitamos los emojis
+comentarios <- gsub("[ \t]{2,}", "", comentarios) # Quitamos tabuladores
+comentarios <- gsub("^\\s+|\\s+$", "", comentarios) # Quitamos tabuladores x2
+comentarios <- tolower(comentarios) # Pasamos todo a minúsculas
+
+corpus_restaurantes <- Corpus(VectorSource(comentarios))
 inspect(corpus_restaurantes[1:3])
 corpus_restaurantes_limpio <- tm_map(corpus_restaurantes, removeWords, stopwords(kind = "es"))
 
@@ -92,5 +112,9 @@ inspect(restaurantes_dtm) # Permite ver cuantas veces aparecen las palabras en e
 restaurantes_stem <- tm_map(corpus_restaurantes_limpio, stemDocument)
 inspect(restaurantes_stem[1:5])
 head(findFreqTerms(restaurantes_dtm, lowfreq=10), 40)
-findAssocs(restaurantes_dtm, 'comida', 0.40)
+findAssocs(restaurantes_dtm, 'comida', 0.90)
 
+
+
+## Clasificación de emociones
+restaurantes_emotions <- classify_emotion(comentarios, algorithm="bayes", prior=1.0)
